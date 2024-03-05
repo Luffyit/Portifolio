d89af6572e2f4111f653dd5c7eb84c5bbe2467aa
lafiti duarte <lafitivianaoliveiraduarte@gmail.com>
2024-03-05 00:16:04 -0300

    add dark-mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
 const mode = document.getElementById('mode-icon')
+const body = document.querySelector('body')
+const header = document.querySelector('header')
+const container = document.querySelector('div.content')
+console.log(container)
             mode.addEventListener('click', clicked)
             function clicked(){
                 if (mode.classList.contains('fa-sun')){
-                mode.classList.remove('fa-sun')
-                mode.classList.add('fa-moon')
-                }else{
-                    mode.classList.remove('fa-moon')
-                    mode.classList.add('fa-sun')
+                mode.classList.toggle('fa-moon')
+                body.classList.toggle('light')
+                header.classList.toggle('light_header')
+                container.classList.toggle('light_content')
                 }
             } 
