cfaa024577290742c5054d95e5c2c3b85c190f96
lafiti duarte <lafitivianaoliveiraduarte@gmail.com>
2024-03-05 02:15:53 -0300

    darkmode uptade

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
 const mode = document.getElementById('mode-icon')
 const body = document.querySelector('body')
 const header = document.querySelector('header')
-const container = document.querySelector('div.content')
-console.log(container)
-            mode.addEventListener('click', clicked)
-            function clicked(){
-                if (mode.classList.contains('fa-sun')){
+const container = document.querySelector('.content')
+const nav = document.querySelectorAll('.a')
+        mode.addEventListener('click', clicked)
+        function clicked(){
+            if (mode.classList.contains('fa-sun')){
                 mode.classList.toggle('fa-moon')
                 body.classList.toggle('light')
                 header.classList.toggle('light_header')
                 container.classList.toggle('light_content')
-                }
-            } 
+                console.log('aqui')
+                for (let c = 0; c < nav.length; c++)
+                    nav[c].classList.toggle('a_light')
+            }
+        } 
