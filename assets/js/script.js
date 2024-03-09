9c8eb12ac3ad3d2ba0c1c35fa7eb7bed127826d1
lafiti duarte <lafitivianaoliveiraduarte@gmail.com>
2024-03-09 12:39:05 -0300

    Better dark mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,12 @@
 const mode = document.getElementById('mode-icon')
-const body = document.querySelector('body')
-const header = document.querySelector('header')
-const nav = document.querySelector('nav.nav-right')
-const links = document.querySelectorAll('.a')
-const container = document.querySelector('.content')
-const contact = document.querySelector('.contact-left')
-const curiosity = document.querySelector('.curiosity')
-const footer = document.querySelector('footer')    
-        mode.addEventListener('click', clicked)
+const root  = document.querySelector(':root')  
+mode.addEventListener('click', clicked)
         function clicked(){
             if (mode.classList.contains('fa-sun')){
                 mode.classList.toggle('fa-moon')
-                body.classList.toggle('light_body')
-                header.classList.toggle('light_header')
-                container.classList.toggle('light_content')
-                nav.classList.toggle('light_nav-right')
-                contact.classList.toggle('light-contact')
-                curiosity.classList.toggle('light-curiosity')
-                footer.classList.toggle('light-footer')
-                for (let c = 0; c < links.length; c++)
-                    links[c].classList.toggle('a_light')
+                root.style.setProperty('--dark-main-color', '#F4F3F7');
+                root.style.setProperty('--white-main-color', '#1C1D26'); 
+                root.style.setProperty('--dark-secondary-color: #0F2027;');
+                root.style.setProperty('--white-secondary-color', '#c8c8ca');
             }
         } 
