535422ddfb398bf0fca5ad33c9c19bb064e9c5e3
lafiti duarte <lafitivianaoliveiraduarte@gmail.com>
2024-03-09 19:46:47 -0300

    optmize dark mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,6 @@ mode.addEventListener('click', clicked)
         function clicked(){
             if (mode.classList.contains('fa-sun')){
                 mode.classList.toggle('fa-moon')
-                root.style.setProperty('--dark-main-color', '#F4F3F7');
-                root.style.setProperty('--white-main-color', '#1C1D26'); 
-                root.style.setProperty('--dark-secondary-color: #0F2027;');
-                root.style.setProperty('--white-secondary-color', '#c8c8ca');
+                root.classList.toggle('light')
             }
         } 
