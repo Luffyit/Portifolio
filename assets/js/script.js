bc4214175626caec20882dbb181c016815912b2d
lafiti duarte <lafitivianaoliveiraduarte@gmail.com>
2024-03-09 20:24:21 -0300

    add copy to clipBoard

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 const mode = document.getElementById('mode-icon')
 const root  = document.querySelector(':root')  
+const copy = document.querySelectorAll('.contact-p')
 mode.addEventListener('click', clicked)
         function clicked(){
             if (mode.classList.contains('fa-sun')){
@@ -7,3 +8,13 @@ mode.addEventListener('click', clicked)
                 root.classList.toggle('light')
             }
         } 
+        function copied(who){
+            console.log(copy)
+            if(who == 'first'){
+                let i = copy[0].textContent
+                navigator.clipboard.writeText(i)
+            }else{
+                let i = copy[1].textContent
+                navigator.clipboard.writeText(i)
+            }
+        }
