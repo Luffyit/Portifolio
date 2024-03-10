9397582e62a5bc82e4cc42cfb9b84ad3273719d5
lafiti duarte <lafitivianaoliveiraduarte@gmail.com>
2024-03-10 20:23:21 -0300

    Better scrolling and CopyBoard

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,30 @@
-const mode = document.getElementById('mode-icon')
-const root  = document.querySelector(':root')  
+// Pegando elementos DOM
+const root  = document.querySelector(':root')
+const mode = document.getElementById('mode-icon')  
 const copy = document.querySelectorAll('.contact-p')
-mode.addEventListener('click', clicked)
+const copyclick = document.querySelectorAll('.fa-copy')
+
+mode.addEventListener('click', clicked) // Event Listner
         function clicked(){
             if (mode.classList.contains('fa-sun')){
                 mode.classList.toggle('fa-moon')
-                root.classList.toggle('light')
+                root.classList.toggle('light') // Troca as cores do root
             }
         } 
-        function copied(who){
-            console.log(copy)
-            if(who == 'first'){
-                let i = copy[0].textContent
-                navigator.clipboard.writeText(i)
+        function copied(who){ // Confere qual a tag clicada
+            if(who == 0){ 
+                navigator.clipboard.writeText(copy[who].textContent) //Copia pro clipboard
+                copyclick[who].style.color = 'var(--details-color)';
             }else{
-                let i = copy[1].textContent
-                navigator.clipboard.writeText(i)
+                navigator.clipboard.writeText(copy[who].textContent)
+                copyclick[who].style.color = 'var(--details-color)';
             }
         }
+        function mouseout(who){ //Cor de confirmação pro copyBoard
+            if(who == 0){
+                copyclick[who].style.color = 'var(--white-primary-color)';
+            }else{
+                copyclick[who].style.color = 'var(--white-primary-color)';
+            }
+        }
+        
